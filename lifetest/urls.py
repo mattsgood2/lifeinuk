60ed60a69b5cef13e6243d35b88a75ae330ab1c0
mattsgood2 <mattsgood2@aol.com>
2025-11-22 20:40:30 +0000

    urls home changed

--- lifetest/urls.py
+++ lifetest/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from bookmode import views as bookmode_views
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quiz.urls')),
+
+    path("", RedirectView.as_view(url="/quiz/practice/", permanent=False)),
+
     path('book_home/', include('bookmode.urls')),
     # path('book_home/', include('bookmode.urls')),
 ]
