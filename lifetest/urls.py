f17ec2b410d24ac47377197ce57f07cd828030b9
mattsgood2 <mattsgood2@aol.com>
2025-11-22 21:34:25 +0000

    testing urls

--- lifetest/urls.py
+++ lifetest/urls.py
@@ -2,8 +2,15 @@ from django.contrib import admin
 from bookmode import views as bookmode_views
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.http import HttpResponse
+
+def health(request):
+    return HttpResponse("HELLO WORKING")
 
 urlpatterns = [
+
+    path('health/', health),
+
     path('admin/', admin.site.urls),
     path('', include('quiz.urls')),
     path('book_home/', include('bookmode.urls')),
